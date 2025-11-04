d6b39d90d042b7863e5f90e743e6f8ef84df4e25
rdk47 <r.d.k.s2005@gmail.com>
2025-11-04 07:21:15 -0300

    Correcion de vite.config.ts para dar permisos a render

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 3000,
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+    allowedHosts: true
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000
   },
   build: {
     outDir: 'dist',
     sourcemap: false
-  },
-  define: {
-    'process.env': {}
   }
 })
